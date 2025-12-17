1f50498873adf8fa4adfeb0d5cfcd1336a7b29f8
Brouse <unaiunai2004@gmail.com>
2025-12-17 19:08:04 +0100

    [UPDATE]: Fixed ids to match arduino

--- isaac-core/src/main/java/es/uib/isaac/map/RoomShape.java
+++ isaac-core/src/main/java/es/uib/isaac/map/RoomShape.java
@@ -1,11 +1,18 @@
 package es.uib.isaac.map;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum RoomShape {
-    SQUARE,
-    CORRIDOR,
-    L_ROOM,
-    LARGE;
+    SQUARE(1),
+    CORRIDOR(2),
+    L_ROOM(3),
+    LARGE(4);
+    private final int value;
     public static RoomShape fromByte(byte b) {
-        return values()[b];
+        for (RoomShape s : RoomShape.values()) {
+            if (s.value == b) return s;
+        }
+        return null;
     }
 }
